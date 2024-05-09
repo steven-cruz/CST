00f3e74abc2f8c24d1e4438745d12f43a8bafab3
samuelCruzTest <samuelnanezcruz995@gmail.com>
2024-05-09 10:16:00 -0500

    - Change the structure for proyect

--- app/index.js
+++ app/index.js
@@ -2,13 +2,13 @@ import express from 'express';
 import {dirname, join} from 'path';
 import { fileURLToPath } from 'url';
 
-import indexRoutes from './src/routes/index.js';
+import indexRoutes from './routes/index.js';
 
 const app = express();
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.set('views', join(__dirname, 'src/views'));
+app.set('views', join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(indexRoutes);
